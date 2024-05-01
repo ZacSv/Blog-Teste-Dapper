945f4533c5a47958e16f221e261ab7368fbcccec
Isac Vieira <isacvieira2009@hotmail.com>
2024-05-01 19:19:00 -0300

    criando a CRUD para os usuários

--- Models/User.cs
+++ Models/User.cs
@@ -8,7 +8,7 @@ namespace Blog.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        public string HashPassword { get; set; }
         public string Bio { get; set; }
         public string Image { get; set; }
         public string Slug { get; set; }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Data.SqlClient;
 using Blog.Models;
 using Dapper.Contrib.Extensions;
 using System.Runtime.InteropServices;
+using System.Xml.Serialization;
 
 namespace Blog
 {
@@ -12,10 +13,14 @@ namespace Blog
         private const string CONNECTION_STRING = @"Server=localhost,1433;Database=BLOG;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=true";
         static void Main(string[] args)
         {
-            ReadUser();
+            //ReadUsers();
+            //ReadUser();
+            //CreateUser();
+            //UpdateUser();
+            DeleteUser();
         }
 
-        public static void ReadUser()
+        public static void ReadUsers()
         {
             using(var connection = new SqlConnection(CONNECTION_STRING))
             {
@@ -26,5 +31,62 @@ namespace Blog
                 }
             }
         }
+        public static void ReadUser()
+        {
+            using(var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+                Console.WriteLine(user.Name);
+            }
+        }   
+        public static void CreateUser()
+        {
+            var user = new User()
+            {
+                Name = "Juscelino",
+                Bio = "Teste de Bio",
+                Email = "juscelinodasgata@gmail.com",
+                HashPassword = "HASH",
+                Image = "Sem-Image",
+                Slug = "juscelino-dasgatas"
+            };
+            using(var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Insert<User>(user);
+                var idUser = user.Id;
+                var busca_no_banco = connection.Get<User>(idUser);
+                Console.WriteLine($"Usuário: {busca_no_banco.Name} inserido no banco com sucesso");
+            }
+        }  
+        public static void UpdateUser()
+        {
+             var user = new User()
+                {
+                    Id = 2,
+                    Name = "Isac",
+                    Bio = "Testando a bio",
+                    Email = "isac@gmail.com",
+                    HashPassword = "HASH",
+                    Image = "https",
+                    Slug = "isac"
+                };
+
+            using(var connection = new SqlConnection(CONNECTION_STRING))
+            {
+               connection.Update<User>(user);
+               Console.WriteLine("Alteração salva");
+            }
+        } 
+        public static void DeleteUser()
+        {
+            using(var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+                connection.Delete<User>(user);
+                Console.WriteLine($"Usuário deletado no banco com sucesso");
+            }
+        }  
+        
     }
 }
+
