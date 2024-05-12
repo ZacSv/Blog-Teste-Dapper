92a84404175b205b29f9d535d595c88fc2768a0c
Isac Vieira <isacvieira2009@hotmail.com>
2024-05-12 11:00:49 -0300

    update

--- obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f00f58230e5f7d514708f49e801ceaf730f47692")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08402e5b26fad75bd79c56f85fef631a036b894b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
